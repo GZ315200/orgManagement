8ae314ea14ed36be183a96004fe70fadc7b7cff5
= <=>
2016-10-02 21:20:51 +0800

    提交机构管理项目

---
+++ src/main/java/com/gyges/love/OrgPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.gyges.love;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrgPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrgPlatformApplication.class, args);
+
+	}
+}
---
+++ src/main/java/com/gyges/love/service/OrgService.java
@@ -0,0 +1,12 @@
+package com.gyges.love.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by gygesM on 2016/10/2.
+ */
+@Service
+public class OrgService {
+
+
+}
